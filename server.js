177a2b0eb9227c57734a9e4a482940d8bae8efa6
Andrew Challen 🦕 <andrew.challen@apdcomms.com>
2019-03-26 20:38:16 +0000

    corrected cors

--- server.js
+++ server.js
@@ -8,16 +8,10 @@ const INDEX = path.join(__dirname, 'index.html');
 const server = express()
     .get(/.*/, function (req, res) {
 
-        res.setHeader('Access-Control-Allow-Origin', 'TRUE');
-        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-//        res.sendfile('socket.io.js');
         res.sendFile(INDEX)
     })
-    // .use(function (req, res, next) {
-    //     res.sendFile(INDEX)
-    // })
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new SocketServer({ server });
