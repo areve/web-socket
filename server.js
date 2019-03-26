fdb635916bab1a718b9147a4ea3d9f2593a82ed1
Andrew Challen 🦕 <andrew.challen@apdcomms.com>
2019-03-26 20:36:34 +0000

    added cors

--- server.js
+++ server.js
@@ -2,26 +2,33 @@ const express = require('express');
 const SocketServer = require('ws').Server;
 const path = require('path');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-  .use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.sendFile(INDEX)
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+    .get(/.*/, function (req, res) {
+
+        res.setHeader('Access-Control-Allow-Origin', 'TRUE');
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+//        res.sendfile('socket.io.js');
+        res.sendFile(INDEX)
+    })
+    // .use(function (req, res, next) {
+    //     res.sendFile(INDEX)
+    // })
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new SocketServer({ server });
 
 wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+    console.log('Client connected');
+    ws.on('close', () => console.log('Client disconnected'));
 });
 
 setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
+    wss.clients.forEach((client) => {
+        client.send(new Date().toTimeString());
+    });
 }, 1000);
